40b177a0abc74ed766be6f4509219c986715c235
GoliasVictor <Dev.jvsb@gmail.com>
2022-12-02 13:20:09 -0300

    Edicao Doacao

--- LivrotecTCC/LivrotecTCC/doacao.aspx.cs
+++ LivrotecTCC/LivrotecTCC/doacao.aspx.cs
@@ -20,15 +20,44 @@ namespace LivrotecTCC
         {
             checarISBNdoacao();
         }
-
+        public bool ValidarCampos()
+        {
+            if (ISBN.Text == "" || Nome.Text == "" || Sinopse.Text == "" || fileUp.PostedFile == null)
+            {
+                erro.Text = "Preencha todos os campos!";
+                return false;
+            }
+            if (ISBN.Text.Length > 10)
+            {
+                erro.Text = "ISBN inválido. Máximo 10 caracteres.";
+                return false;
+            }
+            if (Nome.Text.Length > 100)
+            {
+                erro.Text = "Nome inválido. Máximo 100 caracteres.";
+                return false;
+            }
+            if (Sinopse.Text.Length > 2000)
+            {
+                erro.Text = "Sinopse inválido. Máximo 2000 caracteres.";
+                return false;
+            }
+            string TipoArq = fileUp.PostedFile.ContentType;
+            if (TipoArq != "image/jpeg")
+            {
+                erro.Text = "Arquivo não permitido! Somente Jpeg.";
+                return false;
+            }
+            return true;
+        }
         void checarISBNdoacao()
         {
             if (BD.Livros.consultarISBNDoacao(ISBN.Text) != null)
             {
                 BD.Livros.ConfirmarDoacaoLivroExistente(ISBN.Text);
-                Nome.CssClass = "form-contrl filtroLogin invisivel";
-                Sinopse.CssClass = "form-contrl filtroLogin invisivel";
-                fileUp.CssClass = "invisivel";
+                Nome.Visible = false;
+                Sinopse.Visible = false;
+                fileUp.Visible = false;
                 ISBN.Text = "";
                 Nome.Text = "";
                 Sinopse.Text = "";
@@ -39,35 +68,14 @@ namespace LivrotecTCC
             }
             else
             {
-                Nome.CssClass = "form-contrl filtroLogin";
-                Sinopse.CssClass = "form-contrl filtroLogin";
+                Nome.Visible = true;
+                Sinopse.Visible = true;
+                fileUp.Visible = true;
                 litNome.Text = "Nome Livro";
                 litFotoCapa.Text = "Foto da Capa";
                 litSinopse.Text = "Sinopse";
-                fileUp.CssClass = "nada";
-                if (ISBN.Text != "" && Nome.Text != "" && Sinopse.Text != "" && fileUp.PostedFile != null)
+                if (ValidarCampos())
                 {
-                    string TipoArq = fileUp.PostedFile.ContentType;
-                    if (ISBN.Text.Length > 10)
-                    {
-                        erro.Text = "ISBN inválido. Máximo 10 caracteres.";
-                        return;
-                    }
-                    if (Nome.Text.Length > 100)
-                    {
-                        erro.Text = "Nome inválido. Máximo 100 caracteres.";
-                        return;
-                    }
-                    if (Sinopse.Text.Length > 2000)
-                    {
-                        erro.Text = "Sinopse inválido. Máximo 2000 caracteres.";
-                        return;
-                    }
-                    if (TipoArq != "image/jpeg")
-                    {
-                        erro.Text = "Arquivo não permitido! Somente Jpeg.";
-                        return;
-                    }
                     string NomeArq = Path.GetFileName(fileUp.PostedFile.FileName);
                     BD.Livros.confirmarDoacaoLivroNovo(ISBN.Text, Nome.Text, Sinopse.Text, Nome.Text.Replace(" ", ""));
                     fileUp.PostedFile.SaveAs(Request.PhysicalApplicationPath + @"imagens\" + Nome.Text.Replace(" ", "") + ".jpg");
@@ -81,9 +89,6 @@ namespace LivrotecTCC
                     litSinopse.Text = "";
                     litFotoCapa.Text = "";
                     erro.Text = "";
-                }
-                else {
-                    erro.Text = "Preencha todos os campos!";
                 }
             }
         }
